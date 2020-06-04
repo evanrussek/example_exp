eaa735b09b2670b628fd26bff45905c70af9e0b4
Evan Russek <evrussek@gmail.com>
2020-06-04 18:31:11 +0100

    ...

--- jspsych-6.0.5/evan_plugins/jspsych-evan-explugin.js
+++ jspsych-6.0.5/evan_plugins/jspsych-evan-explugin.js
@@ -1,11 +1,14 @@
 
 jsPsych.plugins["evan-two-stim-choice"] = (function() {
+  // the name above is how we'll reference this
 
   var plugin = {};
 
   plugin.info = {
+    // the name here should be the same
     name: "evan-two-stim-choice",
     parameters: {
+        // these are parameters that the plug_in takes in...
         c1_reward_prob: { // probability of outcome 1 if choice is 1
           type: jsPsych.plugins.parameterType.FLOAT,
           default: undefined
@@ -31,189 +34,128 @@ jsPsych.plugins["evan-two-stim-choice"] = (function() {
 
   plugin.trial = function(display_element, trial) {
 
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////// DEFINE SOME CONSTANTS ///////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
     // keys for left and right
+    // note that placing "var" defines the scope to be within this function
+    // if we don't use var, the variable is global
+    // then we can access it from functions "above" this one.
     var choose_left_key = '1';
     var choose_right_key = '2';
 
-    // screen width and height
+    // get the screen width and height size -
+    // based on their window size
     var parentDiv = document.body;
     var w = parentDiv.clientWidth;
     var h = parentDiv.clientHeight;
 
+    // define structures for variables we might record so that if the trial ends, but we haven't defined these yet, the task doesn't break
+    var response = {
+        rt: null,
+        key: null,
+        key_press_num: null,
+        chosen_side: null,
+      };
+    // define a reward they got which we'll also record
+    var reward = null;
 
-    // fixation parameters
-    var fixation_font_size = h/10;
-    var fixation_x = w/2;
-    var fixation_y = h/2;
-    var fixation_color = "white";
-
-    // place things that are constant throughout the trial
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////// PLACE THE SVG CANVAS AND BACKGROUND ON WHICH WE'll DRAW THINGS ////
+    //////////////////////////////////////////////////////////////////////////
 
     // place the svg -- this is like a canvas on which we'll draw things
-    //  use d3 to chain together commands  -
-    //  this adds lines to the html file which can be viewed in the browser
-    d3.select(".jspsych-content-wrapper")     //  select the part of html in which to place it (the .jspsych-content-wrapper)
+    //  a bit on using d3 for this:
+    // html webpages are split into "divs" that have class names
+    // jspsych creates a webpage that has a "div" called the "content wrapper", which is where the content goes
+    // in the html, it looks like this: <div class="jspsych-content-wrapper">
+    // we reference class names by placing a "." in front of them
+    // select that class and place the svg canvas within it
+    d3.select(".jspsych-content-wrapper")   //  select the part of html in which to place it (.jspsych-content-wrapper is defined by jspsych )
                 .append("svg") // append an svg element
                 .attr("width", w) // specify width and height
                 .attr("height", h)
 
-    // place a grey background rectangle over the whole svg
-    // place grey background on it
+    // place a black background rectangle over the whole svg
+    // here's a link for all the things you can place with d3 (and a whole tutorial on using it)
+    // https://www.dashingd3js.com/svg-basic-shapes-and-d3js
     d3.select("svg").append("rect")
-          .attr("x", 0).attr("y", 0).attr("width", w)
-          .attr("height", h).style("fill", 'gray').style("opacity",.7);
+          .attr("x", 0).attr("y", 0).attr("width", w) // 0, 0 is top left of the "svg" canvas
+          .attr("height", h).style("fill", 'black').style("opacity",.7);
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////// START THE TRIAL ////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    // wait pretrial time sec (this is the ITI), call first part of trial (this is the first thing called after initial display)
+    jsPsych.pluginAPI.setTimeout(function() {
+      display_choice_stim_wait_for_response();
+    }, 1000); // this is where the ITI goes
 
 
-    // place a fixation cross
-    //d3.select('svg').append("text")
-    //                .attr("x",  w/2)
-    //                .attr("y", h/2)
-    //                .attr("font-family","monospace")
-    //                .attr("font-weight","bold")
-    //                .attr("dominant-baseline", "central")
-    //                .attr("font-size", h/10)
-    //                .attr("text-anchor","middle")
-    //                .attr("fill", "white")
-    //                .style("opacity",1)
-    //                .text('+')
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////// FUNCTIONS WHICH RUN the TRIAL ///////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
+    // The functions are:
+    // display_choice_stim_wait_for_response: this places choice stimuli and also sets up a "response handler", which calls the next fun when they respond
+    // handle response: this just records some things about their response, runs some animations and then calls next function
+    // display outcome: this displays the outcome (0 or 1)
+    // end trial: this records the data and shuts down the SVG and ends the trial
 
-
-
-
-    // function to place the choice stims and wait for a response (called at the bottom of plugin.trial)
-
+    // function to place the choice stims and wait for a response (we call this at the bottom)
     var display_choice_stim_wait_for_response = function(){
 
-      var choice_bkg_width = w/4;
+      var choice_bkg_width = w/4; // we can reference w because it's scope is above this function's scope
       var choice_bkg_height = w/4;
       var choice_img_width = w/5;
       var choice_img_height = w/5;
 
-      // place left image background
+      // place a blue rectangle behind the left image
       d3.select("svg").append("rect")
-              .attr("class","choice cL") // this will let us reference the object to change it's color
+              .attr("class","choice cL") // we assign this object a "class" so that we can reference it later (to change it's color)
               .attr("x", w/3 - choice_bkg_width/2)
               .attr("y", h/2 - choice_bkg_height/2)
               .attr("width", choice_bkg_width)
               .attr("height", choice_bkg_height)
               .style("fill", "blue")
-              .style("opacity",1); // set the
+              .style("opacity",1);
 
-        // place right image background
+        // place a blue rectangle behind the right image
         d3.select("svg").append("rect")
-              .attr("class","choice cR") // this will let us reference the object to change it's color
+              .attr("class","choice cR")
               .attr("x", 2*w/3 - choice_bkg_width/2)
               .attr("y", h/2 - choice_bkg_height/2)
               .attr("width", choice_bkg_width)
               .attr("height", choice_bkg_height)
               .style("fill", "blue")
-              .style("opacity",1); // set the
+              .style("opacity",1);
 
-
-        // place choice left image - note how we reference trial.c1_image - this was passed in
+        // place choice left image - note how we reference trial.c1_image - this is the image string that was passed in representing this image
         d3.select("svg").append("svg:image").attr("class", "choice cL").attr("x", w/3 - choice_img_width/2)
             .attr("y", h/2 - choice_img_height/2).attr("width",choice_img_width).attr("height",choice_img_height)
             .attr("xlink:href", trial.c1_image).style("opacity",1);
 
-        // place choice left image - note how we reference trial.c1_image - this was passed in
+        // place choice right image
         d3.select("svg").append("svg:image").attr("class", "choice cR").attr("x", 2*w/3 - choice_img_width/2)
             .attr("y", h/2 - choice_img_height/2).attr("width",choice_img_width).attr("height",choice_img_height)
             .attr("xlink:href", trial.c2_image).style("opacity",1);
 
+        // place text with choice instructions, "choice prompt": this is how you place text...
         if (trial.choice_prompt){
           d3.select("svg").append("text")
                         .attr("class", "choice text")
                         .attr("x", w/2)
                         .attr("y", 7*h/8)
-                        .attr("font-family","monospace")
-                        .attr("font-weight","bold")
+                        .attr("font-family","Helvetica")
+                        .attr("font-weight","light")
                         .attr("font-size",h/40)
                         .attr("text-anchor","middle")
                         .attr("fill", "white")
                         .style("opacity",1)
                         .text('Press 1 for LEFT machine or 2 for RIGHT machine')
         }
-
-
-        // define response handler function // function is automatically called
-        // at response and info has response information
-        var handle_response = function(info){
-
-          // clear timeout counting response time // more relevant for if
-          // a timer was set to limit the response time.
-          jsPsych.pluginAPI.clearAllTimeouts();
-          // kill keyboard listeners
-          if (typeof keyboardListener !== 'undefined') {
-            jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
-          }
-
-          // info is automatically passed into this and has response information
-          if (response.key == null) {
-              response = info;
-          }
-
-          // convert the choice key to a character
-          var choice_char = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(response.key);
-
-          // this references the class name we used when we placed the choice images -
-          // CL is the left stimulus and CR is the right stimulus
-          // we'll use this to change the color of what they chose, and fade out what they ddn't choose
-          if (choice_char == choose_left_key){
-            var chosen_class = '.cL';
-            var unchosen_class = '.cR';
-            // response is a global variable (no var in front of it)
-            // this lets us reference it in the next function
-            response.chosen_side = 1;
-          }
-          else if (choice_char == choose_right_key){
-            var chosen_class = '.cR';
-            var unchosen_class = '.cL';
-            response.chosen_side = 2;}
-          else{console.log('SURPRISE');}
-
-          // select what they choise and change it to red (note this only affects the rect, not the image because that doesn't have a color attribute)
-          d3.select(chosen_class).style('fill',"red");
-          // transition the opacty of what they didn't chcoose to 0 (over 350 milliseconds)
-          d3.selectAll(unchosen_class).transition().style('opacity',0).duration(350)
-
-
-          // wait for some amount of time and then call display outcome
-          jsPsych.pluginAPI.setTimeout(function() {
-              // remove the choice class
-              d3.selectAll(".choice").remove()
-              display_outcome();
-
-            }, 800); // this runs 800 ms after choice is made
-
-        } // end handle response function
-
-        // define function to handle responses that are too slow
-        var handle_slow_response = function(){
-            jsPsych.pluginAPI.clearAllTimeouts();
-
-            // place text 'please respond faster' in red
-            d3.select("svg").append("text")
-                      .attr("class", "outcome")
-                      .attr("x", w/2)
-                      .attr("y", h/2 + w/12)
-                      .attr("font-family","monospace")
-                      .attr("font-weight","bold")
-                      .attr("font-size",w/24)
-                      .attr("text-anchor","middle")
-                      .attr("fill", "red")
-                      .style("opacity",1)
-                      .text('Please response faster')
-
-
-          // record choice as 'slow'
-          response.chosen_side = "SLOW";
-
-          // wait some time and then end trial
-          jsPsych.pluginAPI.setTimeout(function() {
-              end_trial();
-            }, 800); // show slow response for 800 milliseconds
-          } // end handle slow response
 
 
         // define valid responses - these keys were defined above
@@ -228,19 +170,99 @@ jsPsych.plugins["evan-two-stim-choice"] = (function() {
             allow_held_key: false
           });
 
-        // define max response tiem - set timer to wait for that time (this will be turned off when they make a response)
-        var max_response_time = 3000;
+      // define max response tiem - set timer to wait for that time (this will be turned off when they make a response)
+      var max_response_time = 100000; // set to a very large value
+        // wait some time and then end trial
+      jsPsych.pluginAPI.setTimeout(function() {
+          handle_slow_response();
+        }, max_response_time);
+    } // end display_choice_stim_wait for response
+
+    // function to handle responses
+    var handle_response = function(info){
+
+      // clear timeout counting response time // relevant for if
+      // a timer was set to limit the response time.
+      jsPsych.pluginAPI.clearAllTimeouts();
+      // kill keyboard listeners
+      if (typeof keyboardListener !== 'undefined') {
+        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+      }
+
+      // info is automatically passed into this and has response information
+      if (response.key == null) {
+          response = info;
+      }
+
+      // convert the choice key to a character
+      var choice_char = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(response.key);
+
+      // this figures out which image was chosen and which wasn't
+      // record the class name of the chosen image
+      // CL is the left stimulus and CR is the right stimulus
+      // we'll use this to change the color of what they chose, and fade out what they ddn't choose
+      if (choice_char == choose_left_key){ // if they chose left
+        var chosen_class = '.cL';
+        var unchosen_class = '.cR';
+        // response is a global variable (no var in front of it)
+        // this lets us reference it in the next function
+        response.chosen_side = 1;
+      }
+      else if (choice_char == choose_right_key){ // if they chose right
+        var chosen_class = '.cR';
+        var unchosen_class = '.cL';
+        response.chosen_side = 2;}
+      else{console.log('SURPRISE');}
+
+      // select what they choise and change it to red (note this only affects the rect, not the image because that doesn't have a color attribute)
+      d3.select(chosen_class).style('fill',"red");
+      // transition the opacty of what they didn't chcoose to 0 (over 350 milliseconds)
+
+      // we use selectAll because we're selecting multiple items (the image and also the rectangle)
+      d3.selectAll(unchosen_class).transition().style('opacity',0).duration(350)
+
+      // wait for some amount of time and then call display outcome
+      jsPsych.pluginAPI.setTimeout(function() {
+          // remove the choice class
+          d3.selectAll(".choice").remove()
+          display_outcome();
+
+        }, 1000); // this runs 1000 ms after choice is made
+
+    } // end handle response function
+
+    // define function to handle responses that are too slow
+    var handle_slow_response = function(){
+        jsPsych.pluginAPI.clearAllTimeouts();
+
+        // place text 'please respond faster' in red
+        d3.select("svg").append("text")
+                  .attr("class", "outcome")
+                  .attr("x", w/2)
+                  .attr("y", h/2 + w/12)
+                  .attr("font-family","monospace")
+                  .attr("font-weight","bold")
+                  .attr("font-size",w/24)
+                  .attr("text-anchor","middle")
+                  .attr("fill", "red")
+                  .style("opacity",1)
+                  .text('Please response faster')
+
+
+        // record choice as 'slow'
+        response.chosen_side = "SLOW";
+
         // wait some time and then end trial
         jsPsych.pluginAPI.setTimeout(function() {
-            handle_slow_response();
-          }, max_response_time); // show slow response for 800 milliseconds
-      } // end display_choice_stim_wait for response
+            end_trial();
+          }, 1000); // show slow response for 1000 milliseconds
+        } // end handle slow response
 
       // function to display choice outcome.
       var display_outcome = function(){
 
         var reward_probs = [trial.c1_reward_prob, trial.c2_reward_prob];
-        // draw random sample to see whether rewarded
+        // draw random sample to see whether rewarded // note javascript is 0 indexed...
         if (reward_probs[response.chosen_side - 1] < Math.random()){
           var reward_val = 1;
         }else{
@@ -250,7 +272,7 @@ jsPsych.plugins["evan-two-stim-choice"] = (function() {
 
         // wait for some amount of time and display reward
         jsPsych.pluginAPI.setTimeout(function() {
-          // display reward val
+          // display reward value
           d3.select("svg").append("text")
                     .attr("class", "outcome")
                     .attr("x", w/2)
@@ -265,13 +287,12 @@ jsPsych.plugins["evan-two-stim-choice"] = (function() {
 
           }, 500); // this runs wait 500 then show reward
 
+          // wait some time and then end trial
+          jsPsych.pluginAPI.setTimeout(function() {
+              // remove the choice class
+              end_trial();
 
-        // wait some time and then end trial
-        jsPsych.pluginAPI.setTimeout(function() {
-            // remove the choice class
-            end_trial();
-
-          }, 1500); // 1500 msec after display_outcome is called
+        }, 1500); // 1500 msec after display_outcome is called
       } // end display outcome
 
     /// functon to end trial, save data,
@@ -285,37 +306,19 @@ jsPsych.plugins["evan-two-stim-choice"] = (function() {
       // remove the canvas and everthing within it
       d3.select('svg').remove()
 
-      // add correctness answer
-      //console.log('correct: ' + correct_response)
-
       var trial_data = {
         "key_press_num": response.key,
         "chosen_side": response.chosen_side,
         "reward": reward,
         "rt": response.rt,
       };
+      // print this to the browser console
+      console.log(trial_data)
 
       // record data, end trial
       jsPsych.finishTrial(trial_data);
     } // end end trial
-
-    // define the response structure that we'll modify
-    var response = {
-        rt: null,
-        key: null,
-        key_press_num: null,
-        chosen_side: null,
-      };
-      // needs to be predefined in case trial doesn't get response
-      var reward = null;
-
-    // wait pretrial time sec (this is the ITI), call first part of trial (this is the first thing called after initial display)
-    jsPsych.pluginAPI.setTimeout(function() {
-      display_choice_stim_wait_for_response();
-
-    }, 1000); // this is where the ITI goes
-
-  };
+  }; // end plugin.trial
 
   return plugin;
 })();
